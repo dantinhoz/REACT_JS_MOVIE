c00dbef01939e042606f585c03dbef5ecb5e5bdb
dantinhoz <dante.cf.lopes@gmail.com>
2025-08-27 12:29:35 -0300

    primeiro commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import MovieCard from "./components/MovieCard";
+
+function App() {
+  return (
+    <>
+      {/* que tipo de componentes eu preciso? */}
+    </>
+  );
+}
+
+export default App;
